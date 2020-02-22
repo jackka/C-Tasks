fa1f58daf2187bf6177393d20637047f772904cf
Alfar64330 <61142008+Alfar64330@users.noreply.github.com>
2020-02-22 21:23:43 +0300

    Update task1_4.c

--- task1_4.c
+++ task1_4.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 
-char gaps_and_order ( int arr[], int n, char* range){
+char gaps_and_order ( int arr[], int n, char* range) { //n-размер массива, char* range - диапазон ([0-9], [a-z], [A-Z]), см.44 стр
 	printf(" [%s] Range symbol codes Analysis.\n",range );
-	int bk_counter=0;
-	int fd_counter=0;
+	int bk_counter=0; //back counter
+	int fd_counter=0; //forward counter
 	int i;
-	for ( i = 1; i < n; ++i){
+	for ( i = 1; i < n; ++i){ //выясняем, как упорядочены коды символов между собой
 		switch (arr[i-1] - arr[i]){
 		case 1 : 
 			printf("Codes of '%c' and '%c' symbols have reverse order.\n", arr[i-1] , arr[i] );
@@ -22,10 +22,10 @@ char gaps_and_order ( int arr[], int n, char* range){
 	}
 
 	if (fd_counter == n-1 || bk_counter == n-1) {
-		printf("Range from [%s] symbol codes have no gaps.\n We could compare this range as an indivisible piece.\n\n",range);	
+		printf("Range from [%s] symbol codes have no gaps.\n We could compare this range as an indivisible piecec.\n\n",range);	
 		return arr[i-1]; 
 	} else{
-		return -1; printf("No oreder here.\n" );
+		return -1; printf("No order here.\n" );
 	}
 	
 	return -1; printf("\n");
@@ -37,18 +37,18 @@ char gaps_and_order ( int arr[], int n, char* range){
 int main()
 {
 	char space=' ';
-	int arrN[10]={'0','1','2','3','4','5','6','7','8','9'};
-	int arrM[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	int arrL[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	int arr09[10]={'0','1','2','3','4','5','6','7','8','9'};
+	int arraz[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	int arrAZ[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 		
 	char* str_val[4]={" a-z "," A-Z "," 0-9 ","Space"};
 
 	int sort_arr[4];
 
 	sort_arr[0]=' ';
-	sort_arr[1]=gaps_and_order(arrN,10,str_val[2]);	
-	sort_arr[2]=gaps_and_order(arrM,26,str_val[0]);
-	sort_arr[3]=gaps_and_order(arrL,26,str_val[1]);
+	sort_arr[1]=gaps_and_order(arr09,10,str_val[2]);
+	sort_arr[2]=gaps_and_order(arraz,26,str_val[0]);
+	sort_arr[3]=gaps_and_order(arrAZ,26,str_val[1]);
 
 	for (int j = 1; j < 4; j++)
 	{
@@ -72,10 +72,9 @@ int main()
 printf("Each range and Space to its place in order:\n");	
 for (int i = 0; i < 4; ++i)
 {
-	printf("%d",sort_arr[i] );
-	printf("[%s];",str_val[i]);
-}
-printf("\n\n");
+	printf("%d",sort_arr[i] );//число из массива
+	printf("[%s];",str_val[i]);//str_val - имя диапазона ([0-9], [a-z], [A-Z])
+} 
 
 return 0;
 
