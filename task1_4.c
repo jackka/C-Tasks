bd2633928667ec07168643595cffc9d98750eb08
Evgeny <jackki@mail.ru>
2020-02-24 20:32:58 +0300

    Update task1_4.c

--- task1_4.c
+++ task1_4.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-char gaps_and_order ( int arr[], int n, char* range, char** comment) { //n-размер массива, char* range - диапазон ([0-9], [a-z], [A-Z])
-	printf("[%s] Range symbol codes Analysis.\n",range );
-	int bk_counter=0; //backward counter
-	int fd_counter=0; //forward counter
+char* itoa(int val, int base) {
+	static char buf[32] = { 0 };
+	int i = 30;
+	for (; val && i; --i, val /= base)
+		buf[i] = "0123456789abcdef"[val % base];
+	return &buf[i + 1];
+
+}
+
+char gaps_and_order(int arr[], int n, char* range, char** comment) { //n-размер массива, char* range - диапазон ([0-9], [a-z], [A-Z])
+	printf("[%s] Range symbol codes Analysis.\n", range);
+	int bk_counter = 0; //backward counter
+	int fd_counter = 0; //forward counter
 	int i = 1;
 	for (i = 1; i < n; ++i) { //выясняем, как упорядочены коды символов между собой
 		switch (arr[i - 1] - arr[i]) {
@@ -20,25 +31,32 @@ char gaps_and_order ( int arr[], int n, char* range, char** comment) { //n-ра�
 		}
 	}
 
-	if (fd_counter == n-1 || bk_counter == n-1) {
-		printf("Range from [%s] symbol codes have no gaps.\n We could compare this range as an indivisible piece.\n\n",range);	
+	if (fd_counter == n - 1 || bk_counter == n - 1) {
+		printf("Range from [%s] symbol codes have no gaps.\n We could compare this range as an indivisible piece.\n\n", range);
 		char* p = *comment;
-		if (*comment[0] == 32) {
-			comment[0]++; 
-			itoa(arr[0], *comment, 10);
-		}else {
+		if (n == 1) {
+			strcpy(*comment, " ");comment[0]++;
+			strcpy(*comment, itoa(arr[0], 10));
+		}
+		else {
+			strcpy(*comment, "s ");
 			comment[0]++;comment[0]++;
-			itoa(arr[0], *comment, 10); *comment = strcat(*comment, "-");
-			char *tmp = "   ";
-			itoa(arr[i - 1], tmp, 10); 	*comment = strcat(*comment, tmp);
+			strcpy(*comment, itoa(arr[0], 10));
+			*comment = strcat(*comment, "-");
+			char* tmp;
+			tmp = malloc(4 * sizeof(char*));
+			strcpy(tmp, itoa(arr[i - 1], 10));
+			*comment = strcat(*comment, tmp);
+			free(tmp);
 		}
 
 		*comment = p;
-		return arr[i-1]; 
-	} else{
-		return -1; printf("No order here.\n" );
+		return arr[i - 1];
 	}
-	
+	else {
+		return -1; printf("No order here.\n");
+	}
+
 	return -1; printf("\n");
 
 
@@ -46,45 +64,58 @@ char gaps_and_order ( int arr[], int n, char* range, char** comment) { //n-ра�
 
 int main()
 {
-	int arr_space[1] = {' '};
-	int arr09[10]={'0','1','2','3','4','5','6','7','8','9'};
-	int arraz[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	int arrAZ[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	int arr_space[1] = { ' ' };
+	int arr09[10] = { '0','1','2','3','4','5','6','7','8','9' };
+	int arraz[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	int arrAZ[26] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 
-	char* str_val[8] = { "a-z","A-Z","0-9","Space","s       ","s      ","s     ","      " };
+
+	char** str_val;
+	str_val = malloc(8 * sizeof(char*));
+	for (int i = 0; i < 8; i++) 
+		str_val[i] = malloc((8 + 1) * sizeof(char));
+
+	str_val[0] = "a-z";
+	str_val[1] = "A-Z";
+	str_val[2] = "0-9";
+	str_val[3] = "Space";
+	
 
 	int sort_arr[4];
 
-	sort_arr[0]=gaps_and_order(arraz,26,str_val[0],&str_val[0+4]);
-	sort_arr[1]=gaps_and_order(arrAZ,26,str_val[1],&str_val[1+4]);
-	sort_arr[2]=gaps_and_order(arr09,10,str_val[2],&str_val[2+4]);
-	sort_arr[3]=gaps_and_order(arr_space,1,str_val[3],&str_val[3+4]);
+	sort_arr[0] = gaps_and_order(arraz, 26, str_val[0], &str_val[0 + 4]);
+	sort_arr[1] = gaps_and_order(arrAZ, 26, str_val[1], &str_val[1 + 4]);
+	sort_arr[2] = gaps_and_order(arr09, 10, str_val[2], &str_val[2 + 4]);
+	sort_arr[3] = gaps_and_order(arr_space, 1, str_val[3], &str_val[3 + 4]);
 
 	for (int j = 1; j < 4; j++)
 	{
 		for (int i = 0; i < 3; i++)
-		{	
-			if (sort_arr[i]!=-1)
+		{
+			if (sort_arr[i] != -1)
 			{
-				if (sort_arr[i]>sort_arr[i+1]){
+				if (sort_arr[i] > sort_arr[i + 1]) {
 					int x = sort_arr[i]; char* y = str_val[i]; char* z = str_val[i + 4];
 					sort_arr[i] = sort_arr[i + 1]; str_val[i] = str_val[i + 1]; str_val[i + 4] = str_val[i + 5];
 					sort_arr[i + 1] = x; str_val[i + 1] = y; str_val[i + 5] = z;
 				}
-			}else{
+			}
+			else {
 				printf("Ranges are not comparable!\n");
 				return -1;
 			}
 		}
 	}
-	
-// each range + space to its place in order
-printf("Each range and Space to its place in order:\n");	
-    
-	for (int i = 0; i < 4; ++i)	{
-		printf("[%s] have code%s\n",str_val[i],str_val[i+4]);
+
+	// each range + space to its place in order
+	printf("Each range and Space to its place in order:\n");
+
+	for (int i = 0; i < 4; ++i) {
+		printf("[%s] have code%s\n", str_val[i], str_val[i + 4]);
 	}
-    
-return 0;
+
+	free(str_val);
+
+	return 0;
 
 }
